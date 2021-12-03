0b609691a613d82c2dd5436984d7d94958ba30fd
Camp Steiner <joefakocamp@gmail.com>
2021-12-02 22:54:19 -0600

    new 12 by 12

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,32 +1,33 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 2
+#define PIN 7
 
-const int numReadings = 10;
-
-int pixelNums[9][9] = {
-  {0, 1, 2, 3, 4, 5, 6, 7, 8}, 
-  {10, 11, 12, 13, 14, 15, 16, 17, 18}, 
-  {19, 20, 21, 22, 23, 24, 25, 26, 27}, 
-  {29, 30, 31, 32, 33, 34, 35, 36, 37},
-  {39, 40, 41, 42, 43, 44, 45, 46, 47},
-  {49, 50, 51, 52, 53, 54, 55, 56, 57},
-  {59, 60, 61, 62, 63, 64, 65, 66, 67},
-  {69, 70, 71, 72, 73, 74, 75, 76, 77},
-  {79, 80, 81, 82, 83, 84, 85, 86, 87}
+int pixelNums[12][12] = {
+  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+  {23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12},
+  {24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35},
+  {47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36},
+  {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
+  {71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60},
+  {72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83},
+  {95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84},
+  {96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107},
+  {119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108},
+  {120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131},
+  {143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132}
 };
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(150, PIN, NEO_RGB + NEO_KHZ800);
-int readings[numReadings];
-int readIndex = 0;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(144, PIN, NEO_RGB + NEO_KHZ800);
 
 #define ECHO 11
 #define TRIG 13
 
-long duration;
-long distance;
+const int numReadings = 10;
 
-int total = 0;
+int readings[numReadings];     
+int readIndex = 0;             
+int total = 0;                
+int distance = 0;
 
 void setup() {
   pinMode(TRIG, OUTPUT);
@@ -35,6 +36,8 @@ void setup() {
   pixels.setBrightness(100);
   Serial.begin(9600);
 }
+
+long ts = millis();
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -47,38 +50,27 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
 
-  // subtract the last reading:
   total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = pulseIn(ECHO, HIGH);
-  Serial.print(readings[readIndex]*0.034/2);
-  Serial.print(" ");
-  // add the reading to the total:
+  readings[readIndex] = pulseIn(ECHO, HIGH)*0.034/2;
   total = total + readings[readIndex];
-  // advance to the next position in the array:
   readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
   if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
     readIndex = 0;
   }
+  distance = (total / numReadings) - 16.5;
 
-  // calculate the average:
-  distance = (total / numReadings)*0.034/2;
-
-  if(distance < 29 && distance > 1){
+  if (distance <= 40 && distance > 1) {
     int pix = distance/3.35;
     pixels.clear();
     Serial.println(distance);
     Serial.print(" ");
-    for(int i = 0; i<9; i++){
-      pixels.setPixelColor(pixelNums[pix][i], pixels.Color(255-(27*pix), (27*pix), (27*pix)));
+    for(int i = 0; i<12; i++){
+      pixels.setPixelColor(pixelNums[pix][i], pixels.Color(255-(21*pix), (21*pix), (21*pix)));
     }
     pixels.show();
   }
-  else{
-    pixels.clear();
-    pixels.show();
-  }
+//    else{
+//      pixels.clear();
+//      pixels.show();
+//    }
 }
